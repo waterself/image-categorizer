d82d3b3426ed76936281f7c93b65891e673db837
waterself <kimychsw@gmail.com>
2022-09-26 22:55:45 +0900

    초기 커밋.

---
+++ image-categorizer/Core/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace image_categorizer.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool>? _canExecute;
+        private Action<object> value;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Func<object, bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+
+        public event EventHandler CanExcuteChanged
+        { 
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ image-categorizer/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using image_categorizer.Core;
+
+namespace image_categorizer.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand RunViewCommand { get; set; }
+        public RelayCommand SettingViewCommand { get; set; }
+        public RelayCommand SummeryViewCommand { get; set; }
+
+        public RunViewModel RunVM { get; set; }
+        public SummeryViewModel SummerVM { get; set; }
+        public SettingViewModel SettingVM { get; set; }
+
+        private object _currentView;
+
+        public MainViewModel()
+        {
+            RunVM = new();
+            SummerVM = new();
+            SettingVM = new();
+
+            CurrentView = RunVM;
+
+            RunViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RunVM;
+            });
+
+            SummeryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SummerVM;
+            });
+            SettingViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingVM;
+            });
+
+        }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView= value;
+                OnPropertyChanged();
+            }
+        }
+
+
+    }
+}
